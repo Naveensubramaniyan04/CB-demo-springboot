5f6b56ca1d1151ed3a753e8574eb0c596f28ff6f
naqsh01 <shnaqvi@gmail.com>
2024-10-22 12:53:11 -0400

    Create a new flag for test

--- src/main/java/org/springframework/samples/petclinic/Flags.java
+++ src/main/java/org/springframework/samples/petclinic/Flags.java
@@ -7,6 +7,7 @@ public class Flags  implements RoxContainer {
 
 	public RoxFlag enableTutorial = new RoxFlag(false);
 	public final RoxString titleColors = new RoxString("White", new String[] { "White", "Blue", "Green", "Yellow" });
+	public final RoxString shozabColors = new RoxString("White", new String[] { "White", "Blue", "Green", "Yellow" });
 	public final RoxInt titleSize = new RoxInt(14, new int[] { 14, 18, 24 });
 	public final RoxDouble specialNumber = new RoxDouble(3.14, new double[]{ 2.71, 0.577 });
 	public final RoxEnum<Color> titleColorsEnum = new RoxEnum<>(Color.WHITE);
